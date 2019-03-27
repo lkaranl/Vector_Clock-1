62d7b925a3e8d0e14ed00f23fefada295656257a
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2019-03-27 01:25:00 -0400

    Update INSTALL_ARCH_64.sh

--- INSTALL_ARCH_64.sh
+++ INSTALL_ARCH_64.sh
@@ -10,7 +10,7 @@ if [ -d ${HOME}/Área\ de\ Trabalho/ ]; then
     cd ${HOME}/Área\ de\ Trabalho/clock
 fi
 
-yes | sudo pacman -S python2-dateutil python2-netifaces python2-ipaddr
+yes | sudo wget pacman -S python2-dateutil python2-netifaces python2-ipaddr
 
 
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/gstreamer0.10.tar.gz
@@ -23,31 +23,31 @@ tar -xvf gstreamer0.10.tar.gz
 cd gstreamer0.10
 yes | makepkg -si
 cd ..
-rm gstreamer0.10.tar.gz
+rm -r gstreamer0.10.tar.gz
 
 tar -xvf gstreamer0.10-base.tar.gz
 cd gstreamer0.10-base
 yes | makepkg -si
 cd ..
-rm gstreamer0.10-base.tar.gz
+rm -r gstreamer0.10-base.tar.gz
 
 tar -xvf wxgtk2.8.tar.gz
 cd wxgtk2.8
 yes | makepkg -si
 cd ..
-rm wxgtk2.8.tar.gz
+rm -r wxgtk2.8.tar.gz
 
 tar -xvf wxpython-gtk2.tar.gz
 cd wxpython-gtk2
 yes | makepkg -si
 cd ..
-rm wxpython-gtk2.tar.gz
+rm -r wxpython-gtk2.tar.gz
 
 tar -xvf wxpython2.8.tar.gz
 cd wxpython2.8
 yes | makepkg -si
 cd ..
-rm wxpython2.8.tar.gz
+rm -r wxpython2.8.tar.gz
 
 
 exec bash
